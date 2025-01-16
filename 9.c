92c481a4b2911dbb44440ac4c4557d731f1a07b8
Kea135 <kea1352001@gmail.com>
2025-01-16 22:23:10 +0330

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,20 @@
+
+#include <mega16.h>
+ 
+#include <delay.h>
+  flash char digite [16]={0x3F, 0x06,0x5B,0x4F,0x66,0x6D,
+  0x7D,0x07, 0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
+ unsigned char i;
+void main(void)
+{
+DDRC=0xFF;
+PORTC=0x00;
+while (1)
+    {
+    for (i=0; i<10; i++)
+    {
+    PORTC= digite [i];
+          delay_ms(1000);
+    };
+    };
+}
